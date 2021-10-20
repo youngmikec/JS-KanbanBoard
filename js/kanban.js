9eefc7c8937ce3670abf207485600e0bebeb64cf
youngmikec <michaelozor15@gmail.com>
2021-10-20 10:53:40 +0100

    first commit, done 7 out of 15

--- js/kanban.js
+++ js/kanban.js
@@ -16,6 +16,36 @@ add_btn.addEventListener('click', () => {
 });
 
 const create_item = () => {
+  let item = document.createElement('div');
+  item.setAttribute('id', 'item-' + order);
+  item.classList.add('item');
+
+  item.addEventListener('dragstart', (event) => {
+    event.dataTransfer.setData('text', event.target.id);
+  });
+
+  item.addEventListener('dragend', (event) => {
+    event.dataTransfer.clearData();
+  });
+
+  let input = document.createElement('input');
+  item.appendChild(input);
+
+  let save_btn = document.createElement('button');
+  save_btn.innerHTML = 'Save';
+  save_btn.addEventListener('click', () => {
+    error.innerHTML = '';
+    if(input.value !== ''){
+      order += 1;
+      item.innerHTML = input.value;
+      adding = false;
+    }else{
+      error.innerHTML = message;
+    }
+  });
+
+  item.appendChild.save_btn;
+  return item;
 };
 
 document.querySelectorAll('.drop').forEach(element => {
